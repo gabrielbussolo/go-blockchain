9d7e05af43293a532f1720d3bffdefe0f391932b
Gabriel Bussolo <contact@gabrielbussolo.dev>
2021-12-13 12:06:33 -0300

    Refactor blockchain to receive hash interface and calculate a PoW

--- blockchain/core.go
+++ blockchain/core.go
@@ -3,22 +3,42 @@ package blockchain
 import (
 	"crypto/sha256"
 	"go-chain/blockchain/block"
+	"go-chain/blockchain/hash"
 	"time"
 )
 
-var chain []block.Block
+type blockchain struct {
+	chain []block.Block
+	hash  hash.Hash
+}
 
-func Save(proof uint, previousHash [sha256.Size]byte) {
+func New(hash hash.Hash) *blockchain {
+	return &blockchain{
+		chain: make([]block.Block, 0),
+		hash:  hash,
+	}
+}
+
+func (b *blockchain) Save(proof uint, previousHash [sha256.Size]byte) {
 	newBlock := block.Block{
-		Index:        uint(len(chain) + 1),
+		Index:        uint(len(b.chain) + 1),
 		Timestamp:    time.Now(),
 		Proof:        proof,
 		PreviousHash: previousHash,
 	}
-	chain = append(chain, newBlock)
+	b.chain = append(b.chain, newBlock)
 }
 
-func GetPreviousBlock() block.Block {
-	lastBlockIndex := len(chain) - 1
-	return chain[lastBlockIndex]
+func (b *blockchain) GetPreviousBlock() block.Block {
+	lastBlockIndex := len(b.chain) - 1
+	return b.chain[lastBlockIndex]
+}
+
+func (b *blockchain) ProofOfWork(previousProof uint) uint {
+	if previousProof == 0 {
+		panic("previous proof should be bigger than 0")
+	}
+	var newProof uint = 1
+	const factor uint = 2
+	return b.hash.Calculate(previousProof, newProof, factor)
 }
--- blockchain/core_test.go
+++ blockchain/core_test.go
@@ -3,19 +3,30 @@ package blockchain
 import (
 	"crypto/sha256"
 	"go-chain/blockchain/block"
+	"go-chain/blockchain/hash"
 	"testing"
 )
 
+type hashMock struct {
+	calculateMock func(previousProof, newProof, factor uint) uint
+}
+
+func (h *hashMock) Calculate(previousProof, newProof, factor uint) uint {
+	return h.calculateMock(previousProof, newProof, factor)
+}
+
 func TestSave(t *testing.T) {
+	blockchain := New(hash.New())
 	sum := sha256.Sum256([]byte("sha256 from previous block"))
-	Save(1, sum)
-	if len(chain) == 0 {
+	blockchain.Save(1, sum)
+	if len(blockchain.chain) == 0 {
 		t.Errorf("size of the chain should be 1")
 	}
 }
 
 func TestGetPreviousBlock(t *testing.T) {
-	chain = []block.Block{
+	blockchain := New(hash.New())
+	blockchain.chain = []block.Block{
 		{
 			Index: 0,
 		},
@@ -27,9 +38,37 @@ func TestGetPreviousBlock(t *testing.T) {
 		},
 	}
 
-	previousBlock := GetPreviousBlock()
+	previousBlock := blockchain.GetPreviousBlock()
 
 	if previousBlock.Index != 2 {
 		t.Errorf("the index of the block should be 2")
 	}
+}
+
+func TestProofOfWork(t *testing.T) {
+	hashMock := &hashMock{calculateMock: func(previousProof, newProof, factor uint) uint {
+		return 3545
+	}}
+	blockchain := New(hashMock)
+
+	t.Run("fail when previous proof is 0", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Errorf("it was expecting to panic when the previousProof is %d", 0)
+			}
+		}()
+		expected := 0
+		work := blockchain.ProofOfWork(0)
+		if work != 0 {
+			t.Errorf("proof of work should not be different from %d", expected)
+		}
+	})
+
+	t.Run("process the proof", func(t *testing.T) {
+		var expected uint = 3545
+		got := blockchain.ProofOfWork(2)
+		if got != expected {
+			t.Errorf("proof of work should not be different from %d", expected)
+		}
+	})
 }
